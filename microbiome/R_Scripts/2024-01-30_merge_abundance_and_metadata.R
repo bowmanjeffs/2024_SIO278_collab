7b7ee1ed750d5bacb50238bd860b7d6f1bcb5b3e
Riley Hale <haleriley@gmail.com>
2024-01-30 13:19:49 -0800

    add options for reduced dataframe in R script

--- microbiome/R_Scripts/2024-01-30_merge_abundance_and_metadata.R
+++ microbiome/R_Scripts/2024-01-30_merge_abundance_and_metadata.R
@@ -14,9 +14,13 @@ setwd("2024_SIO278_collab-main/microbiome/")
 
 metadata <- read_excel("metadata_SRP128128.xlsx")
 
-asv.abund.df <- read.csv("microbiome_ASVs.csv")
+asv.abund.df.full <- read.csv("microbiome_ASVs.csv")
+asv.abund.df.reduced <- read.csv("microbiome_ASVs_reduced.csv")
 
 # ---- format and merge ----
+
+# asv.abund.df <- asv.abund.df.full
+asv.abund.df <- asv.abund.df.reduced # pick which file to work with
 
 asv.abund.df <- t(asv.abund.df)
 colnames(asv.abund.df) <- asv.abund.df[1,]
@@ -25,7 +29,9 @@ asv.abund.df <- as.data.frame(asv.abund.df[-1,])
 asv.abund.df$Run <- rownames(asv.abund.df)
 
 combined.df <- merge(metadata, asv.abund.df, by = "Run")
-saveRDS(combined.df, file = "2024-01-30_combined_abund_metadata.rds")
 
+# ---- save as RDS ----
+# saveRDS(combined.df, file = "2024-01-30_combined_abund_metadata.rds")
+saveRDS(combined.df, file = "2024-01-30_combined_abund_metadata_reduced.rds")
 
 
